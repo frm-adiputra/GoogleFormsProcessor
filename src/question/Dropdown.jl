fdd6a47136a099163739dc51cab17b378304dc5b
Firmansyah Adiputra <frm.adiputra@gmail.com>
2020-04-07 14:41:30 +0700

    Add percentages

--- src/question/Dropdown.jl
+++ src/question/Dropdown.jl
@@ -7,5 +7,6 @@ function generate(q::Dropdown, df::DataFrame)::Vector{QuestionSpecResult}
 end
 
 function describe(q::Dropdown, df::DataFrame)
-    by(df, q.name, N = q.name => length)
+    ntot = nrow(df)
+    by(df, q.name, N = q.name => length, P = [q.name] => x -> (length(x[q.name])/ntot) * 100)
 end
